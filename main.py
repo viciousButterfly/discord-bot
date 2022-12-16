b4afa0c45275e4c00bfaa9737cbed1666afdfe67
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-12-16 17:24:26 +0530

    Added hello and env

---
+++ main.py
@@ -0,0 +1,23 @@
+import nextcord
+from nextcord.ext import commands
+import os
+from dotenv import load_dotenv
+
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello! {ctx.author.mention}")
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user.name}")
+
+
+if __name__ == "__main__": 
+    load_dotenv()
+    bot.run(os.getenv("DISCORD_TOKEN"))  
