b446c9321489292558ea3e23544910f7a95ae26c
Tanmay Patil <tanmaynpatil105@gmail.com>
2022-12-20 13:20:49 +0530

    Embedded github response and added comments

--- main.py
+++ main.py
@@ -12,40 +12,68 @@ intents = nextcord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
-# greeting command
+#
+# @returns greeting message
+#
 @bot.command()
 async def hello(ctx):
     await ctx.send(f"Hello! {ctx.author.mention}")
 
-# socials command to display socials
+#
+# @returns cofsug socials
+#
 @bot.command()
 async def socials(ctx):
     await ctx.send("*Checkout our socials :* \n**Instagram** : https://www.instagram.com/cofsug/ \n**LinkedIn** : https://www.linkedin.com/company/coep-s-free-software-users-group/ \n\n")
 
-# foss command to scrap itsfoss's recent blog
+#
+# @returns scraps itsfoss's recent blog
+#
 @bot.command(name="foss")
 async def article(ctx):
     alt,href = scrapper.itsfoss()
     await ctx.send("{}\n{}".format(alt,href))
 
-# github command to find top5 repos for the given argument
+#
+# @returns information about cofsug
+#
+@bot.command(name="cofsug")
+async def cofsug(ctx):
+    await ctx.send("Write info about cofsug!")
+
+#
+# @returns finds top5 repos for the given argument
+#
 @bot.command(name="github")
 async def repositories(ctx,arg):
+
     repos = github.scrape(arg)
-    message = "*Here are Top {} repos:*".format(len(repos))
+    names = []
+    links = []
 
     for item in repos:
-        message+="\n⦁ Name : {}\n\t`Link` : {}".format(item[0],item[1])
+        names.append(item[0])
+        links.append(item[1])
+    
+    names = "\n".join(map(str,names)) 
+    links = "\n".join(map(str,links)) 
 
-    await ctx.send(message)
+    embed = nextcord.Embed(title="Github",color=nextcord.Color.green())
+    embed.add_field(name="Name", value=names, inline="true")
+    embed.add_field(name="Link", value=links, inline="true")
 
-# poll command to create a poll
+
+    await ctx.send(embed=embed)
+
+#
+# @returns creates a poll
+#
 @bot.command(name="poll")
 async def poll(ctx,choice1,choice2,*,topic):
     IST = pytz.timezone('Asia/Kolkata')
-    embed = nextcord.Embed(title=topic, description=f"1️⃣\t{choice1}\n2️⃣\t{choice2}",timestamp=datetime.datetime.now(IST))
+    embed = nextcord.Embed(title=topic, description=f"1️⃣\t{choice1}\n2️⃣\t{choice2}",timestamp=datetime.datetime.now(IST),color=nextcord.Color.green())
     embed.set_footer(text=f"Poll by {ctx.author.name}")
+    embed.set_thumbnail(url="https://media.discordapp.net/attachments/1035419482476269598/1036862785365221406/Untitled_design1.png?width=581&height=436")
     react = await ctx.send(embed=embed)
 
     await react.add_reaction("1️⃣")
@@ -53,7 +81,10 @@ async def poll(ctx,choice1,choice2,*,topic):
 
     await ctx.message.delete()
 
-# clear command to clear messages (only for role with name='Admin')
+
+#
+# @returns clear messages (only for role with name='Admin')
+#
 @bot.command()
 @commands.has_role('Admin')
 async def clear(ctx, amount = 5):
@@ -64,7 +95,9 @@ async def clear(ctx, amount = 5):
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
 
-# Handling errors
+#
+# @handles Handlind errors
+#
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.NoPrivateMessage):
@@ -78,7 +111,9 @@ async def on_command_error(ctx, error):
     elif isinstance(error, commands.CommandNotFound):
         await ctx.send("*This command is not listed in my dictionary.*")
 
-# Running the bot
+#
+# @Running the bot
+#
 if __name__ == "__main__": 
     load_dotenv()
     bot.run(os.getenv("DISCORD_TOKEN"))  
